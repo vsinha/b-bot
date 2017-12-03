41dcf0de122f0c296bff836a6117f8a8e461426d
Ian Whitlock <ian@theknown.net>
2017-12-02 23:17:06 -0600

    Fix over9000 script

--- src/scripts/over9000.ts
+++ src/scripts/over9000.ts
@@ -4,16 +4,16 @@ class Over9000 {
 
     public registerListener(robot: any) {
         return robot.hear(/[\d.,]+/g, (res: any) => {
-            const num: string = res.match[0].split(',').join('').split('.')[0];
-            if (parseInt(num, 10) < 9000) {
-                return;
-            }
             const prob = Math.random();
             if (prob < 0.9) {
                 return;
             }
-            const message = Over9000.baseMessage;
-            res.send(message);
+
+            const num: string = res.match[0].split(',').join('').split('.')[0];
+            if (parseInt(num, 10) > 9000) {
+                const message = Over9000.baseMessage;
+                res.send(message);
+            }
         });
     }
 }
